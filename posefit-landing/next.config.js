0b993beb1e900fd18698688d78aeba2a684775b9
Your Name <your.email@example.com>
2025-05-10 14:33:58 +0300

    Remove manual webpack configuration

--- posefit-landing/next.config.js
+++ posefit-landing/next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
   assetPrefix: '/PoseFit_project/',
   images: { unoptimized: true },
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig; 
